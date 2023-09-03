827ea7001c31bd3ed08f4ef8fc3d7f249e5fe4e8
Tsvetochkin <143913127+Tsvetochkin@users.noreply.github.com>
2023-09-02 21:19:27 -0300

    Add files via upload

---
+++ src/test/java/com/tsvetkov/petProject/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsvetkov.petProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
